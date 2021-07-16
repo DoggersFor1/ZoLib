eb5e39ad4b16d3bd95e1602827854c8efb8e4c89
Xgod2205 <61017490+Xgod2205@users.noreply.github.com>
2021-07-16 17:49:11 -0500

    Update ZoLib.js
    changed ZoLib

--- ZoLib.js
+++ ZoLib.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name        ZoLib
+// @namespace   The first zombs.io script library
+// @match       http://zombs.io/
+// @grant       none
+// @version     beta
+// @author      -
+// @description the first zombs.io library
+// ==/UserScript==
+
 class addChatListner {
     constructor(func, msg, allowedUsers = localStorage.getItem('displayName')) {
         game.network.addRpcHandler("ReceiveChatMessage", (message) => {
